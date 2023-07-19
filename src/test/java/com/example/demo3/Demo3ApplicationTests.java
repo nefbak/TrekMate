17e0a61c29da35c2b9e39638b845f1dc99ed40ba
fSanvictores <135087609+fSanvictores@users.noreply.github.com>
2023-07-18 19:35:39 -0700

    Update Demo3ApplicationTests.java

--- src/test/java/com/example/demo3/Demo3ApplicationTests.java
+++ src/test/java/com/example/demo3/Demo3ApplicationTests.java
@@ -1,41 +1,41 @@
-package com.example.demo3;
+// package com.example.demo3;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.http.MediaType;
+// import org.springframework.test.context.junit.jupiter.SpringExtension;
+// import org.springframework.test.context.junit4.SpringRunner;
+// import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.demo3.controllers.UsersController;
-import com.example.demo3.models.User;
-import com.example.demo3.models.UserRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import static com.jayway.jsonpath.JsonPath.*;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+// import com.example.demo3.controllers.UsersController;
+// import com.example.demo3.models.User;
+// import com.example.demo3.models.UserRepository;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import static com.jayway.jsonpath.JsonPath.*;
+// import org.springframework.test.web.servlet.MvcResult;
+// import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 // import org.junit.runner.RunWith;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+// import static org.hamcrest.CoreMatchers.*;
+// import static org.hamcrest.MatcherAssert.assertThat;
 
-import static org.assertj.core.api.Assertions.assertThat;
+// import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
+// import java.util.List;
 
-@SpringBootTest
-class Demo3ApplicationTests {
+// @SpringBootTest
+// class Demo3ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
 /*@SpringBootTest
 class Demo3ApplicationTests {
 
@@ -142,3 +142,17 @@ public class UsersControllerTest {
                 .andExpect(jsonPath("$.author", is("Harper Lee")));
     }
 } */
+
+package com.example.demo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
