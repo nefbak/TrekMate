e2272a7010ce0014048fd87ef80a49815259d3ce
nefbak <134996637+nefbak@users.noreply.github.com>
2023-07-25 10:51:34 -0700

    Update ReviewsController.java

--- src/main/java/com/example/demo3/controllers/ReviewsController.java
+++ src/main/java/com/example/demo3/controllers/ReviewsController.java
@@ -2,12 +2,10 @@ package com.example.demo3.controllers;
 
 import java.util.List;
 import java.util.Map;
-
 import java.security.Provider.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.example.demo3.models.Review;
 import com.example.demo3.models.ReviewRepository;
 import com.example.demo3.models.User;
