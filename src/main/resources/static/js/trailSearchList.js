3f9f024d31f0c0575dc89166d70b932af10899e4
nefbak <134996637+nefbak@users.noreply.github.com>
2023-07-31 17:15:33 -0700

    Update trailSearchList.js

--- src/main/resources/static/js/trailSearchList.js
+++ src/main/resources/static/js/trailSearchList.js
@@ -1,5 +1,5 @@
 let suggestions = ['Grouse Grind', 'The Chief','Garibaldi Lake','Stawamus Chief','', 'Coquitlam Crunch' ,'Joffre Lakes', 
                    "Saint Mark's Summit",'Quarry Rock', 'Dog Mountain Trail', 'Kennedy Falls', 'Cypress Provincial Park', 
-                   'Cabin Lake', 'Eqgle Bluffs', 'Shoreline Trail','Norvan Falls','Gold Creek Lower Falls', "Brother's Creek Trailhead",
+                   'Cabin Lake', 'Eagle Bluffs', 'Shoreline Trail','Norvan Falls','Gold Creek Lower Falls', "Brother's Creek Trailhead",
                   'Mount Seymour Provincial Park', 'Baden Powell Trail', 'Lynn Headwaters Regional Park', 'Big Cedar', 'Thunderbird Ridge', 
                   'Mount Seymour Trail', 'Mount Gardner Trail Head'];
